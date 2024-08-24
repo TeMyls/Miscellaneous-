83d5271377e636053281b5a9067ab16af5a009a9
Terrance Myles Jr <123514008+TeMyls@users.noreply.github.com>
2024-08-24 17:09:33 -0500

    Add files via upload

--- Wave Function Collapse/wfc.py
+++ Wave Function Collapse/wfc.py
@@ -4,8 +4,7 @@ import time
 #import console
 #outside of pythonista
 import os
-
-#0 any: Its the starting 
+#0 any
 #it is a blamk cell that can be anything
 #1 water
 #only boarders sand and itself
@@ -16,210 +15,194 @@ import os
 #4 dirt
 #only boarders trees, sand and itself
 
+print("1 is water","2 is sand","3 is tree","4 is dirt", sep = "\n")
 
-options = {1:[1,2],2:[1,2,4],3:[3,4],4:[2,3,4]}
+#left,down,right,up
+
+blank_cell = 0
+water_cell = 1
+sand_cell = 2
+tree_cell = 3
+dirt_cell = 4
 
 
+X = [0,1,0,-1]
+Y = [-1,0,1,0]
 
-def empty_map(rows,columns,empty_arr):
-	for i in range(rows):
-		empty_arr.append([0]*columns)
-			
-	return  empty_arr
-			
-def cleaner(arr):
-   
-	for r in range(len(arr)):
-		for c in range(len(arr[r])):
-			#cardinal directions
-			n, nw, w, sw, s, se, e, ne = 0,0,0,0,0,0,0,0
-			current = arr[r][c]
-			if c > 0:
-				w = arr[r][c - 1] 
+options = {
+	water_cell:[water_cell,sand_cell],
+	sand_cell:[water_cell,sand_cell,dirt_cell],
+	tree_cell:[tree_cell,dirt_cell],
+	dirt_cell:[sand_cell,tree_cell,dirt_cell]
+				}
+				
+		
+
+colors = {
+
+	blank_cell:'⬜️',
+	water_cell:'🟦',
+	sand_cell:'🟧',
+	tree_cell:'🟩',
+	dirt_cell:'🟫'
+	
+	
+	
+}
 					
-			if c < len(arr[r]) - 1:
-				e = arr[r][c + 1] 
-			if r > 0:
-				n = arr[r - 1][c]
-				if c > 0:
-					nw = arr[r - 1][c - 1] 
-				if c < len(arr[r]) - 1:
-					ne = arr[r - 1][c + 1] 
+
+
+
+def empty_map(rows,columns,color):
+	return [[color]*columns for i in range(rows)]
+			
+def in_bounds(x, y,grid_w,grid_h):
+		return 0 <= x < grid_w and 0 <= y < grid_h
 				
-			if r < len(arr) - 1:
-				s = arr[r + 1][c]
-				if c > 0:
-					sw = arr[r + 1][c - 1] 
-				if c < len(arr[r]) - 1:
-					se = arr[r + 1][c + 1] 
-			quick_arr = [n, nw, w, sw, s, se, e, ne]	
-			if quick_arr.count(1) >= 6:
-				arr[r][c] = 1
-			if quick_arr.count(4) + quick_arr.count(3) > 5 and current == 2:
-				arr[r][c] = 4
 				
+
 
 def set_seed_tile(arr):
-	
 	#position of starting tileoit
 	row,col = random.randint(0,len(arr)-1), random.randint(0,len(arr[0])-1)
 	#giving an initial value a random integer within our dictionary to represent a starting tile
 	val = random.randint(1, len(options))
-	#print(val,'\n',row,col)
-	
+	#print(val,'\n',row,col
 	arr[row][col] = val
 	return row, col
 
-			
+
 
 def display(arr):
-	
-	for i in arr:
-		print(i)
-		
-
-		
-	
-		
-def symbol_display(arr):
 	h=''
 	for i in arr:
 		for j in i:
-			
-			
-			if j == 0:
-				h = h + '⬜️'
-			if j == 1:
-				h = h + '🟦'
-			if j == 2:
-				h = h +  '🟧'
-			if j == 3:
-				h = h +  '🟩'
-			if j == 4:
-				h = h +  '🟫'
-			
+				h = h +  colors[j]
 		h = h + '\n'
 	print(h)
-
-
-def shared(ls1,ls2,ls3 = None,ls4 = None):
-	#returns the shared elements of two list
-	#for three elemnents
-	#why so many loops? 
-	#the loops make the weighs matter
-	#[1,2,2,2,3] and [2,3] will return
-	#[2,2,2,3] instead of [2,3]
-	repeats_1 = ls1.count(1) + ls1.count(2) + ls1.count(3) + + ls1.count(4)
-	repeats_2 = ls2.count(1) + ls2.count(2) + ls2.count(3) + + ls2.count(4)
-	#for two elemnents
-	if repeats_1 > repeats_2:
-		return [x for x in ls1 if x in ls2]
-	elif repeats_1 < repeats_2: 
-		return [x for x in ls2 if x in ls1]
-	else:
-		return [x for x in ls1 if x in ls2]
-
-
-def restrict(cell_val1, cell_val2 = None):
-	#collapse the options based on cell adjency
-	#returns a random element from the new list to add to the array
-	shared_vals = []
-	
-	if cell_val2:
-		#for two values
-		shared_vals = shared(options[cell_val1],options[cell_val2])
-		choice = random.randint(0, len(shared_vals) - 1)
-		return shared_vals[choice]
-	else:
-		#for one value
-		#just makes a random choice based on the cell value
-		#returns random choice
-		neighbors = options[cell_val1]
-		#print(neighbors)
-		choice = random.randint(0,len(neighbors) - 1)
-		return options[cell_val1][choice]
-		
-	
 	
 
 	
 def flood_fill_collapse(arr, row, col):
 	w = len(arr)
 	h = len(arr[0])
+	#direction vectors in array
+	#[up,right,down,left]
+	x_vectors = [0,-1,0,1]
+	y_vectors = [-1,0,1,0]
 	#queue
 	t = 0
 	visited = []
 	kyu = []
-	kyu.append((row,col))
+	kyu.append([row,col])
+	
 	while kyu:
 		cur = kyu.pop(0)
-		if cur[0] >= w or cur[0] < 0 or cur[1] >= h or cur[1] < 0:
-			continue
-		elif (cur[0],cur[1]) not in visited:
-			visited.append((cur[0],cur[1]))
+		
+		if in_bounds(cur[1],cur[0],w,h) and  [cur[0],cur[1]] not in visited:
+			cur_value = arr[cur[0]][cur[1]]
+			
+			visited.append([cur[0],cur[1]])
 			t += 1
 			
-			right, left, top, bottom = 0,0,0,0
-			if cur[1] > 0:
-				right = arr[cur[0]][cur[1] - 1] 
 			
-			if cur[1] < h - 1:
-				left = arr[cur[0]][cur[1] + 1] 
-			if cur[0] > 0:
-				top = arr[cur[0]  - 1][cur[1]]			
-			if cur[0] < w - 1:
-			  bottom = arr[cur[0] + 1][cur[1]]
-			#print()
+			
+			#possible future values of X and Y depening on position
+			possibilities = []
+			
+			
+			for i in range(len(x_vectors)):
+				y = cur[0] + y_vectors[i]
+				x = cur[1] + x_vectors[i]
+				
+				
+				if in_bounds(x,y,w,h):
+					neighbor = arr[y][x]
+					if neighbor != blank_cell:
+						possibilities.append(options[arr[y][x]])
+					
+				
+			
+			
+			
+			choice = 0
+			#print(possibilities)
+			
+			if len(possibilities) > 1:
+				
+				
+				#using set
+				shared = set(possibilities[0])
+				#print(shared)
+				for arrays in possibilities:
+					#set intersection
+					#only keeps the shared elements 
+					shared = shared & set(arrays)
+				possibilities = list(shared)
+				'''
+				#using dictionary
+				shared = {}
+				common = []
+				for arrays in possibilities:
+					for ele in arrays:
+						if shared.get(ele):
+							shared[ele] += 1
+						else:
+							shared.update({ele:1})
+				for key in shared:
+					if shared[key] == len(possibilities):
+						common.append(key)
 						
-			#two cells are filled and neighbor is affected
-			if right != 0 and bottom == 0 and left == 0 and top != 0:
-				arr[cur[0]][cur[1]] = restrict(right,top)
+				print(common)
+				possibilities = common
+				'''
+						
 				
-			if right != 0 and bottom != 0 and left == 0 and top == 0:
-				arr[cur[0]][cur[1]] = restrict(right,bottom)
 				
-			if right == 0 and bottom == 0 and left != 0 and top != 0:
-				arr[cur[0]][cur[1]] = restrict(left,top)
 				
-			if right == 0 and bottom != 0 and left != 0 and top == 0:
-				arr[cur[0]][cur[1]]= restrict(left,bottom)
-			#one cell is filled and neighbor is affected
-			if right != 0 and bottom == 0 and left == 0 and top == 0:
-				arr[cur[0]][cur[1]] = restrict(right)
+				choice = random.randint(0,len(possibilities) - 1)
+				print("yep")
 				
-			if right == 0 and bottom != 0 and left == 0 and top == 0:
-				arr[cur[0]][cur[1]] = restrict(bottom)
 				
-			if right == 0 and bottom == 0 and left == 0 and top != 0:
-				arr[cur[0]][cur[1]] = restrict(top)
+				arr[cur[0]][cur[1]] = possibilities[choice]
 				
-			if right == 0 and bottom == 0 and left != 0 and top == 0:
-				arr[cur[0]][cur[1]] = restrict(left)
+					
 				
-			kyu.append((cur[0] + 1, cur[1]))
-			kyu.append((cur[0] - 1, cur[1]))
-			kyu.append((cur[0], cur[1] + 1))
-			kyu.append((cur[0], cur[1] - 1))
-			#console.clear()
-
-      
-			os.system('cls')
-			symbol_display(arr)
-			time.sleep(.001)
+				
+			elif len(possibilities) == 1:
+				#options[arr[cur[0]][cur[1]]]
+				
+				choice = random.randint(0,len(possibilities) - 1)
+				
+				possibilities = possibilities[choice]
+				
+				choice = random.randint(0,len(possibilities) - 1)
+					
+				print("neo")
+				arr[cur[0]][cur[1]] = possibilities[choice]
+			
+			
+			kyu.append([cur[0] + 1, cur[1]])
+			kyu.append([cur[0] - 1, cur[1]])
+			kyu.append([cur[0], cur[1] + 1])
+			kyu.append([cur[0], cur[1] - 1])
+		#console.clear()
+		os.system('cls')
+		
+		display(arr)
+		time.sleep(.15)
 	#console.clear()
 	os.system('cls')
-	cleaner(arr)
-	symbol_display(arr)
-	#print(t)
+	
+	display(arr)
+
 			
 			
 def wave_function_collapse(rows,cols):
-	arena = empty_map(rows,cols,[])
+	arena = empty_map(rows,cols,blank_cell)
 	x,y = set_seed_tile(arena)
-	
 	flood_fill_collapse(arena,x,y)
-	display(arena)
-	#print(arena)
 	
-wave_function_collapse(20,20)
-print("1 is water","2 is sand","3 is tree","4 is dirt", sep = "\n")
+	#display(arena)
+	
+wave_function_collapse(18,18)
