a9f9c65e67a1ecf900bdad786b5fbcf69bb8b490
Terrance Myles Jr <123514008+TeMyls@users.noreply.github.com>
2024-09-11 12:13:32 -0500

    Add files via upload

--- Matrix Math/MatrixMath.py
+++ Matrix Math/MatrixMath.py
@@ -77,6 +77,38 @@ def matrix_addition(a_matrix, b_matrix):
 	for i in result:
 		print(i)
 	return result
+	
+def scalar_multiply(scalar, matrix):
+	rows = len(matrix)
+	cols = len(matrix[0])
+	
+	
+	result = new_matrix(rows,cols)
+	
+	for y in range(rows_a):
+		product = 0
+		for x in range(cols_a):
+			product = matrix[y][x] * scalar
+			
+	for i in result:
+		print(i)
+	return result
+	
+def scalar_divide(scalar, matrix):
+	rows = len(matrix)
+	cols = len(matrix[0])
+	
+	
+	result = new_matrix(rows,cols)
+	
+	for y in range(rows_a):
+		product = 0
+		for x in range(cols_a):
+			product = matrix[y][x] / scalar
+			
+	for i in result:
+		print(i)
+	return result
 			
 			
 def matrix_subtraction(a_matrix, b_matrix):
@@ -168,6 +200,61 @@ def matrix_multiply(a_matrix, b_matrix):
 	display(result)
 	return result		
 		
+		
+def matrix_multiply_2(a_matrix, b_matrix):
+	rows_a = len(a_matrix)
+	cols_a = len(a_matrix[0])
+	rows_b = len(b_matrix)
+	cols_b = len(b_matrix[0])
+	print(f"A rows: {rows_a} A cols: {cols_a}")
+	display(a_matrix)
+	print(f"B rows: {rows_b} B cols: {cols_b}")
+	display(b_matrix)
+
+	if cols_a != rows_b and rows_a != cols_b:
+		print("Matrix \"A\"s columns must be equal to Matrix \"B\"s rows")
+		return 
+	elif rows_a == 0 or rows_b == 0 or cols_a == 0 or cols_a == 0:
+		print("Empty Matrix")
+		return 
+	
+	#display(a_matrix)
+	#display(b_matrix)
+	
+	#result = new_matrix(min(rows_b,rows_a),min(cols_a,cols_b))
+	
+	result = []
+	if rows_a == cols_b or cols_a == rows_b:
+		result = []
+		a = []
+		b = []
+		if rows_a == cols_b:
+			a = a_matrix
+			b = b_matrix
+			result = new_matrix(rows_b,cols_a)
+		elif cols_a == rows_b:
+			a = b_matrix
+			b = a_matrix
+			result = new_matrix(rows_a,cols_b)
+			
+		for ax in range(len(a[0])):
+			#result.append([])
+			for by in range(len(b)):
+				product = 0
+				for bx in range(len(b[0])):
+					print(a[bx][ax]," times ", b[by][bx])
+					product += a[bx][ax] * b[by][bx]
+				#result[ax].append(product)
+				result[by][ax] = product
+				
+			#print()
+	
+		
+			
+	display(result)
+	return result						
+		
+		
 matrix_a = [
 	[2, 1, 1, 1, 1]
 	]
@@ -192,9 +279,9 @@ matrix_ap = [
 	]
 
 matrix_xyz = [
-		[1],
+		[0],
 		[2],
-		[3]
+		[6]
 	]
 	
 matrix_xy = [
@@ -202,9 +289,9 @@ matrix_xy = [
 		[2]
 	]
 
-matrix_multiply(matrix_ap,matrix_xyz)
+matrix_multiply_2(matrix_xyz,ortho_projection)
 print()
-matrix_multiply(ortho_projection, matrix_xyz)
+matrix_multiply_2(ortho_projection, matrix_xyz)
 #matrix_multiply(z_rotate3d(math.pi/4),matrix_xyz)
 #matrix_addition(matrix_a,matrix_ap)
 	
