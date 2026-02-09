22d26a7898dd5389d8f773414b234777fe2888b6
T Myles Jr <tlmyles5@hotmail.com>
2026-02-09 04:32:19 -0600

    Update bezier.py

--- Bezier Curves/bezier.py
+++ Bezier Curves/bezier.py
@@ -1,6 +1,6 @@
 import math
 
-floor = lambda x: math.floor(x)
+floor = lambda x: round(x)
 
 floor_color = 1
 
@@ -10,12 +10,12 @@ other_color = 9
 
 colors = {
 	#wall
-	wall_color:' .',
+	wall_color:'.',
 	#floor
-	floor_color:' #',
+	floor_color:'#',
 	#player
-	player_color:' @',
-	other_color:' X'
+	player_color:'@',
+	other_color:'X'
 
 	
 	
@@ -56,12 +56,24 @@ def r_bez(point_ls: list[float], points: [int], point: int) -> list[float]:
 	# 4 X-Y coordinate pairs have 3 lines between them
 	# when a value is connected inbetween these 3 lines sequentially you get 2 lines, then 1 line fron where each point on the found
 	# this recurcise and iterative versions do exactly that, once the amount of points is reduced to 4 numbers, 2 X-Y pairs, a line
-			# the curve's coordinate at that percentage is found'
+			# the curve's coordinate at that percentage is found
+			
 	ln = len(point_ls)
-	
+	#print(point_ls)
 	if ln == 4:
-		return point_ls
+		x1 = point_ls[0]
+		y1 = point_ls[1]
 		
+		x2 = point_ls[2]
+		y2 = point_ls[3]
+	
+		#print(x1, y1, x2, y2)
+		
+		x = lerp(x1, x2, point/points)
+		y = lerp(y1, y2, point/points)
+			
+		return [x , y]
+				
 	if ln < 6:
 		return
 
@@ -81,19 +93,15 @@ def r_bez(point_ls: list[float], points: [int], point: int) -> list[float]:
 		ls_point.append(
 			lerp(y1, y2, point/points)
 			)
-	
+
 	return r_bez(ls_point, points, point)
 	
 def i_bez(point_ls: list[float], points: [int], point: int) -> list[float]:
 	
 	ln = len(point_ls)
 	
-	if ln == 4:
-		return point_ls
-		
 	if ln < 6:
 		return
-
 	
 	ls_point = [coord for coord in point_ls]
 	while len(ls_point) != 4:
@@ -121,29 +129,40 @@ def i_bez(point_ls: list[float], points: [int], point: int) -> list[float]:
 		#print(len(ls_point))
 		#if len(ls_point) == 4:
 			#return ls_point
+	x1 = ls_point[0]
+	y1 = ls_point[1]
 	
-	return ls_point
+	x2 = ls_point[2]
+	y2 = ls_point[3]
+
+	#print(x1, y1, x2, y2)
+	
+	x = lerp(x1, x2, point/points)
+	y = lerp(y1, y2, point/points)
+	return [x , y]
 
 		
 		
 grid = empty_map(22, 22, wall_color)
 w = len(grid[0]) - 1
 h = len(grid) - 1
-points = 8
+points = 10
 coords = [
 			#X 									Y
 			0									, 0, 
-			floor(w  * 0.25)	,floor(h * .25), 
-			floor(w  * 0.75)	,floor(h * .25), 
-			w									,floor(h * .75)
+			floor(w  * 0.20)	,floor(h * .80), 
+			floor(w  * 0.65)	,floor(h * .20), 
+			floor(w  * 0.90)	,floor(h * .75)
 		]
 #curve = n_curve_bezier(points, 8)
+
 for i in range(points):
-	curve = i_bez(coords, points, i)
-	for j in range(0, len(curve), 2):
-		x = floor(curve[j])
-		y = floor(curve[j + 1])
-		grid[y][x] = floor_color
+	curve = r_bez(coords, points, i)
+	
+	x = floor(curve[0])
+	y = floor(curve[1])
+	
+	grid[y][x] = floor_color
 	print(curve)
 
 #symbol_display(grid)
@@ -152,7 +171,8 @@ for i in range(0, len(coords), 2):
 		x = floor(coords[i])
 		y = floor(coords[i + 1])
 		
-		grid[y][x] = other_color		
+		grid[y][x] = player_color
+
 symbol_display(grid)
 print(len(curve))
 
