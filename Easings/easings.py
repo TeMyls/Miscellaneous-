f6ac173fea4de2b54c0019b6b3f77d3e9ade10fa
T Myles Jr <tlmyles5@hotmail.com>
2026-02-17 16:15:20 -0600

    Update easings.py

--- Easings/easings.py
+++ Easings/easings.py
@@ -16,6 +16,7 @@ def linear(t):
 	t = clamp(t, 0.0, 1.0)
 	return t
 
+
 def easeInSine(t):
 	t = clamp(t, 0.0, 1.0)
 	return 1 - math.cos((t * math.pi) / 2)
@@ -27,6 +28,7 @@ def easeOutSine(t):
 def easeInOutSine(t):
 	t = clamp(t, 0.0, 1.0)
 	return -(math.cos(math.pi * t) - 1) / 2
+
 
 def easeInQuad(t):
 	t = clamp(t, 0.0, 1.0)
@@ -42,6 +44,7 @@ def easeInOutQuad(t):
 		return 2 * t * t
 	return 1 - math.pow(-2 * t + 2, 2) / 2
 
+
 def easeInCubic(t):
 	t = clamp(t, 0.0, 1.0)
 	return t * t * t
@@ -55,6 +58,7 @@ def easeInOutCubic(t):
 	if t < 0.5:
 		return 4 * t * t * t
 	return 1 - math.pow(-2 * t + 2, 3) / 2
+
 
 def easeInQuart(t):
 	t = clamp(t, 0.0, 1.0)
@@ -70,6 +74,7 @@ def easeInOutQuart(t):
 		return 8 * t * t * t * t
 	return 1 - math.pow(-2 * t + 2, 4) / 2
 
+
 def easeInQuint(t):
 	t = clamp(t, 0.0, 1.0)
 	return  t * t * t * t * t
@@ -83,6 +88,7 @@ def easeInOutQuint(t):
 	if t < 0.5:
 		return 16 * t * t * t * t * t
 	return 1 - math.pow(-2 * t + 2, 5) / 2
+
 
 def easeInExpo(t):
 	t = clamp(t, 0.0, 1.0)
@@ -106,6 +112,7 @@ def easeInOutExpo(t):
 		return math.pow(2, 20 * t - 10) / 2
 	return (2 - math.pow(2, -20 * t + 10)) / 2
 
+
 def easeInCirc(t):
 	t = clamp(t, 0.0, 1.0)
 	return 1 - math.sqrt(1 - math.pow(t, 2))
@@ -119,6 +126,7 @@ def easeInOutCirc(t):
 	if t < 0.5:
 		return (1 - math.sqrt(1 - math.pow(2 * t, 2))) / 2
 	return (math.sqrt(1 - math.pow(-2 * t + 2, 2)) + 1) / 2
+
 
 def easeInBack(t):
 	t = clamp(t, 0.0, 1.0)
@@ -140,6 +148,7 @@ def easeInOutBack(t):
 		return (math.pow(2 * t, 2) * ((c2 + 1) * 2 * t - c2)) / 2
 	return (math.pow(2 * t - 2, 2) * ((c2 + 1) * (t * 2 - 2) + c2) + 2) / 2
 
+
 def easeInElastic(t):
 	t = clamp(t, 0.0, 1.0)
 	c4 = (2 * math.pi) / 3
@@ -149,7 +158,6 @@ def easeInElastic(t):
 		return 1.0
 	return -math.pow(2, 10 * t - 10) * math.sin((t * 10 - 10.75) * c4)
 	
-
 def easeOutElastic(t):
 	t = clamp(t, 0.0, 1.0)
 	c4 = (2 * math.pi) / 3
@@ -170,6 +178,73 @@ def easeInOutElastic(t):
 		return -(math.pow(2, 20 * t - 10) * math.sin((20 * t - 11.125) * c5)) / 2
 	return (math.pow(2, -20 * t + 10) * math.sin((20 * t - 11.125) * c5)) / 2 + 1
 
-def interpolate(a, b, percentage):
-	return a + (b - a) * linear(percentage)
+def easeOutBounce(t):
+	n1 = 7.5625
+	d1 = 2.75
+
+	if t < (1 / d1):
+		return n1 * t * t
+	elif t < (2 / d1):
+		t = t - 1.5 / d1
+		return n1 * t * t + 0.75
+	elif t < (2.5 / d1):
+		t = t - 2.25 / d1
+		return n1 * t * t + 0.9375
+	else:
+		t = t - 2.625 / d1
+		return n1 * t * t + 0.984375
+
+def easeInBounce(t):
+	return 1 - easeOutBounce(1 - t)
+
+def easeInOutBounce(t):
+	if t < 0.5:
+		return (1 - easeOutBounce(1 - 2 * t)) / 2
+	return (1 + easeOutBounce(2 * t - 1)) / 2
+
+easings = {
+		"linear": linear,
+
+		"easeInSine": easeInSine,
+		"easeInQuad": easeInQuad,
+		"easeInCubic": easeInCubic,
+		"easeInQuart": easeInQuart,
+		"easeInQuint": easeInQuint,
+		"easeInExpo": easeInExpo,
+		"easeInCirc": easeInCirc,
+		"easeInBack": easeInBack,
+		"easeInElastic": easeInElastic,
+		"easeInBounce":easeInBounce,
+
+		"easeOutSine": easeOutSine,
+		"easeOutQuad": easeOutQuad,
+		"easeOutCubic": easeOutCubic,
+		"easeOutQuart": easeOutQuart,
+		"easeOutQuint": easeOutQuint,
+		"easeOutExpo": easeOutExpo,
+		"easeOutCirc": easeOutCirc,
+		"easeOutBack": easeOutBack,
+		"easeOutElastic": easeOutElastic,
+		"easeOutBounce":easeOutBounce,
+
+		"easeInOutSine": easeInOutSine,
+		"easeInOutQuad": easeInOutQuad,
+		"easeInOutCubic": easeInOutCubic,
+		"easeInOutQuart": easeInOutQuart,
+		"easeInOutQuint": easeInOutQuint,
+		"easeInOutExpo": easeInOutExpo,
+		"easeInOutCirc": easeInOutCirc,
+		"easeInOutBack": easeInOutBack,
+		"easeInOutElastic": easeInOutElastic,
+		"easeInOutBounce":easeInOutBounce
+	
+	}
+
+def interpolate(a, b, percentage, ease_type = "linear"):
+	return a + (b - a) * easings[ease_type](percentage)
+
+print(interpolate(3, 7, 0.73, "linear"))
+print(interpolate(3, 7, 0.73, "easeInBack"))
+print(interpolate(3, 7, 0.73, "easeInOutElastic"))
+
 
