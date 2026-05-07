0069cacdff78ac61896118bbe5f27a4cb2a5947a
T Myles Jr <tlmyles5@hotmail.com>
2026-05-06 20:54:48 -0500

    Update sudoku.py

--- Sudoku/sudoku.py
+++ Sudoku/sudoku.py
@@ -79,67 +79,149 @@ def clear_console():
 		os.system('cls')
 		#on pythonista
 		#console.clear()
-		
-		
 
-
-def get_faux2D(array, row, col, rows, cols):
-	#indexing a 1D array as if it were a 2D array
-	return array[(row % rows) * cols + (col % cols)]
-	
-def set_faux2D(array, row, col, rows, cols, value):
-	#indexing a 1D array as if it were a 2D array
-	array[(row % rows) * cols + (col % cols)] = value
-	return array
-	
-def show_faux(array, rows, cols):
+def animate(wait, grid):
+		if wait:
+			time.sleep(.05)
+		#os.system('cls')
+		console.clear()
+		display_true(grid)
+		
+			
+			
+def quadrant_sweep(grid, x, y, choices):
+# Note: in other languages the double slash is equivalent to math.floor(x/n)
+	# this chunk sweeps through the 3x3 box of one of the quadrants checking quadrant neighbors
+	# sudoku has 3x3 quadrants 
+	# below considers 3x3 quadrants
+	qx = (x//3) 
+	qy = (y//3) 
+			
+	ox = qx * 3
+	oy = qy * 3
+	#print(f"X: {x}, Y: {y}, QX: {qx} QY: {qy} ")
 	s = ""
-	for y in range(rows):
+	
+	a = 0
+	while a < 9:
+		s = s + " OX: " + str(ox) + " OY: " + str(oy) + " G: " + str(grid[oy][ox])
+		s = s + "\n"
 		
-		for x in range(cols):
-			s += str(get_faux2D(array, y, x, rows, cols)) + " "
-		s += "\n"
-	return s
 	
+		
+		#grid[oy][ox] = x + y
+		if grid[oy][ox] in choices:
+			choices.remove(grid[oy][ox])
+			
+		
+		
+		if (a + 1) % 3 == 0:
+			oy += 1
+			ox = ox - 3
+		
+		ox += 1
+		a += 1
+		
+	#print(s)
+			
+			
+			
+			
+	return choices
 	
+def cross_sweep(grid, x, y, choices):
+	w = 9
+	h = 9
+	# considers individual rows and columns
+	i, j, k, l = y, y, x, x
+	# treats the 1D array as a 2D array
+	# in lua this would be 1 to 9
+	cond = (
+
+			i != 0 or 
+			j != h - 1 or
+			k != 0 or
+			l != w - 1
+			
+		)
+		
+	while cond:
+		if i != 0:
+			i = i - 1
+			
+		if j != w - 1:
+			j = j + 1
+			
+		if k != 0:
+			k = k - 1
+			
+		if l != h - 1:
+			l = l + 1
+			
+		
+		#grid[i][x] = 2
+		#grid[j][x] = 4
+		
+		#grid[y][k] = 1
+		#grid[y][l] = 3
+			
+		# can be replace with indexing a 2D array
+		top = grid[i][x]
+		
+		bottom = grid[j][x]
+		
+		left = grid[y][k]
+		
+		right = grid[y][l]
+	
+		
+		if top in choices:
+			choices.remove(top)
+			
+		if bottom in choices:
+			choices.remove(bottom)
+			
+		if left in choices:
+			choices.remove(left)
+			
+		if right in choices:
+			choices.remove(right)
+		
+		#print("x y top bottom left right")
+		#print(x, y, top, bottom, left, right)
+		#print(i, j, k, l)
+		cond = (
+			i != 0 or 
+			j != h - 1 or
+			k != 0 or
+			l != w - 1
+		)
+		
+			
+	return  choices
+
 def get_sudoku_board():
 	"""
-	# in lua add 1 to all
-	_____________________________
-	|0  1  2  |3  4  5  |6  7  8 |
-	|9  10 11 |12 13 14 |15 16 17|                     
-	|18 19 20 |21 22 23 |24 25 26|
-	|_________|_________|________|     
-	|27 28 29 |30 31 32 |33 34 35|
-	|36 37 38 |39 40 41 |42 43 44|
-	|45 46 47 |48 49 50 |51 52 53|
-	|_________|_________|________|
-	|54 55 56 |57 58 59 |60 61 62|
-	|63 64 65 |66 67 68 |69 70 71|
-	|72 73 74 |75 76 77 |78 79 80|
-	|_________|_________|________|
+					0          1        2		  QX
+			 0  1  2    3  4  5   6  7  8	X
+			_____________________________
+		0	|         |         |        |
+	0 1	|         |         |        |                     
+		2	|         |         |        |
+			|_________|_________|________|     
+		3	|         |         |        |
+	1 4	|         |         |        |                     
+		5	|         |         |        |
+			|_________|_________|________|
+		6	|         |         |        |
+	2 7	|         |         |        |                     
+		8	|         |         |        |
+			|_________|_________|________|
+ QY Y
 	"""
 	# the nine 3x3 quadrants a sudoku board is made of
 	w = 9
 	h = 9
-	
-	region_test = [
-		j for j in range(w * h) 
-	]
-	
-	board = [
-		0 for j in range(w * h) 
-	]
-	#print(show_faux(region_test, w, h))
-	#print(len(region_test))
-	quads = [
-		0, 3, 6,
-		27, 30, 33,
-		54, 57, 60,
-	]
-	
-	
-	
 	
 	
 	grid = empty_map(9, 9, 0)
@@ -151,163 +233,57 @@ def get_sudoku_board():
 	col = random.randint(0, w - 1)
 	#print(row, col, sep = " ")
 	
-			
-	for y in range(h):
-		for x in range(w):
+	#iteration tracker
+	q = 0
+	
+	y = 0
+	while y < 9:
+		x = 0
+		restarts = 0
+		while x < 9:
+			q += 1
 			choices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-			# Note: in other languages the double slash is equivalent to math.floor(x/n)
-			# sudoku has 3x3 quadrants 
-			# in this case I alternate between indexing the array as a 2D arry and a 1D array
-			# below considers 3x3 quadrants
-			qx = x//3
-			qy = y//3
 			
-			idx = qx * 3 + qy * 27
+			if grid[y][x] in choices:
+				choices.remove(grid[y][x])
 			
-			ox = qx * 3
-			oy = qy * 3
+			choices = cross_sweep(grid, x, y, choices)
 			
+			choices = quadrant_sweep(grid, x, y, choices)
 			
-			#print(qx * 3, qy * 3)
-			s_1 = ""
-			s_2 = "" #f"OX:{ox} OY:{oy} "
-			
-			a = 0
-			while a < 9:
-				# offset by itself only outputs the
-				# first 3x3 quadrant
-				# 0  1  2
-				# 9  10 11
-				# 18 19 20
-				# using idx it gets the correct position of indexing in a 1D array
-				offset = ((a//3) * 9) + a % 3
-				
-				s_2 += str(get_faux2D(board, oy, ox, h, w)) + " "
-				
-				#if board[idx + offset] in choices:
-					#choices.remove(board[idx + offset])
-				
-				if get_faux2D(board, oy, ox, h, w) in choices:
-					#choices.remove(get_faux2D(board, oy, ox, h, w))
-					choices.remove(grid[oy][ox])
-				
-				#grid[oy][ox] = 8
-				# for 2D arrays
-				ox += 1
-				if (a + 1) % 3 == 0:
-					oy += 1
-					ox = qx * 3
-				
-				#print(f"OX:{ox} OY:{oy} ")
-				
-				
-				
-				
-					
-				s_1 += str(idx + offset) + " "
-				
-				if (a + 1) % 3 == 0:
-					s_1 += "\n"
-					s_2 += "\n"
-				#print(offset)
-				a += 1
-			#print(s_1)
-			#print(s_2)
-			
-		
-			
-			
-			#print(x, y, qx, qy, offset)
-			
-			
-			
-			
-			
-			
-			# considers individual rows and columns
-			i, j, k, l = y, y, x, x
-			# treats the 1D array as a 2D array
-		  # in lua this would be 1 to 9
-			cond = (
-
-					i != 0 or 
-					j != h - 1 or
-					k != 0 or
-					l != w - 1
-				)
-			while cond:
-				if i != 0:
-					i = i - 1
-					
-				if j != w - 1:
-					j = j + 1
-					
-				if k != 0:
-					k = k - 1
-					
-				if l != h - 1:
-					l = l + 1
-					
-				
-				#grid[i][x] = 2
-				#grid[j][x] = 4
-				
-				#grid[y][k] = 1
-				#grid[y][l] = 3
-					
-				# can be replace with indexing a 2D array
-				top = grid[i][x]
-				#get_faux2D(board, i, x, h, w)
-				bottom = grid[j][x]
-				#get_faux2D(board, j, x, h, w)
-				left = grid[y][k]
-				#get_faux2D(board, y, k, h, w)
-				right = grid[y][l]
-				get_faux2D(board, y, l, h, w)
-				
-				if top in choices:
-					choices.remove(top)
-					
-				if bottom in choices:
-					choices.remove(bottom)
-					
-				if left in choices:
-					choices.remove(left)
-					
-				if right in choices:
-					choices.remove(right)
-				
-				#print("x y top bottom left right")
-				#print(x, y, top, bottom, left, right)
-				#print(i, j, k, l)
-				cond = (
-					i != 0 or 
-					j != h - 1 or
-					k != 0 or
-					l != w - 1
-				)
-				
-			#print(choices, y, x)
 			if choices:
-				#set_faux2D(board, y, x, h, w, random.choice(choices))
+				
 				grid[y][x] = random.choice(choices)
-			#break
-		#break
+				x += 1
+			else:
+				# grid[y][x] is 0, no choice was made
+				# erase entire row back to zero
+				# mn = x - 2
+				while x != 0:
+					x -= 1
+					grid[y][x] = 0
+					
+				restarts += 1
+				if restarts > 15:
+					grid = empty_map(9, 9, 0)
+					restarts = 0
+					y = 0
+			#animate(True, grid)
+			#display_true(grid)
+		
+		y += 1
+	
 
+		
 		
 	display_true(grid)
-	#print(show_faux(board, w, h))
-	#BFS flood fill
-	#queue
-	
-	#display_true(grid)
+	print(f"iterations: {q}")
 	return grid
+
 
 
 def game_loop(chances, shown_cells):
 	#if in_bounds(x, y, len(grid[0]), len(grid):
-	
-	revealed_cells = shown_cells
 	
 	visible_grid = empty_map(9, 9, -1)
 	hidden_grid = get_sudoku_board()
@@ -316,7 +292,7 @@ def game_loop(chances, shown_cells):
 	#display_board(visible_grid)
 
 
-	uncovered_all = revealed_cells == 9 * 9 
+	uncovered_all = 81 - shown_cells == 0
 	mine_neighbors = 0
 	
 	has_moved = False
@@ -367,7 +343,7 @@ def game_loop(chances, shown_cells):
 				visible_grid[cur_y][cur_x] = n
 				#display_board(visible_grid)
 				#display_board(hidden_grid)
-				revealed_cells += 1
+				shown_cells += 1
 			else:
 				chance_missed = True
 				chances -= 1
@@ -377,7 +353,7 @@ def game_loop(chances, shown_cells):
 				
 			
 			
-			uncovered_all = revealed_cells == mine_neighbors 
+			uncovered_all = 81 - shown_cells == 0
 			if uncovered_all:
 				clear_console()
 				display_board(visible_grid)
@@ -386,6 +362,7 @@ def game_loop(chances, shown_cells):
 			if chance_missed == chances:
 				print("You lost")
 				break 
+				
 		except Exception as e:
 			print(e)
 			break
@@ -393,12 +370,12 @@ def game_loop(chances, shown_cells):
 	if input("Play Again? Y/N?").upper() == "Y":
 		
 		chances = int(input("Enter a chance count: "))
-		if not type(chances) == type("chance"):
+		if not type(chances) == type(3):
 			print("Invalid Chances")
 			return
 		
-		chances = abs(int(chances))
-		game_loop(chances, 80)
+		
+		game_loop(chances, 8)
 
-game_loop(5, 80)
+game_loop(5, 8)
 #get_sudoku_board()
